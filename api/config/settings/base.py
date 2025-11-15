506fcd597d3d8a8df70345bb6017140d09be2905
Seud <seudsahm1@gmail.com>
2025-11-15 17:33:18 +0300

    chore: create and configure local apps

--- api/config/settings/base.py
+++ api/config/settings/base.py
@@ -45,7 +45,9 @@ THIRD_PARTY_APPS = [
 ]
 
 LOCAL_APPS = [
-    
+    "core_apps.common",
+    "core_apps.user_auth",
+    "core_apps.user_profile",
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
